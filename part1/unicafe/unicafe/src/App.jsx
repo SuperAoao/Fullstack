7b2cc60c42925869b4ba64e6df03be7aa3edb6c2
Ao Zhang <superaoao39@outlook.com>
2026-02-06 16:57:47 +1100

    1.11

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 
 const StatisticLine = (props) => {
   return (
-    <p>{props.text} {props.value}</p>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 // a proper place to define a component
@@ -19,12 +22,17 @@ const Statistics = (props) => {
     return (
       <div>
         <h2>statistics</h2>
-        <StatisticLine text="good" value={props.good} />
-        <StatisticLine text="neutral" value={props.neutral} />
-        <StatisticLine text="bad" value={props.bad} />
-        <StatisticLine text="all" value={props.good + props.neutral + props.bad} />
-        <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
-        <StatisticLine text="positive" value={props.good / (props.good + props.neutral + props.bad) * 100} />
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={props.good} />
+            <StatisticLine text="neutral" value={props.neutral} />
+            <StatisticLine text="bad" value={props.bad} />
+            <StatisticLine text="all" value={props.good + props.neutral + props.bad} />
+            <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
+            <StatisticLine text="positive" value={props.good / (props.good + props.neutral + props.bad) * 100} />
+          </tbody>
+        </table>
+
       </div>
     )
   }
