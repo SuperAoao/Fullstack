8af3961899957aa604bec130650ce53eaf1f3bab
Ao Zhang <superaoao39@outlook.com>
2026-02-07 22:08:02 +1100

    2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,52 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+
+
+const Content = (props) => (
+  <div>
+    {props.parts.map((part) => (
+      <Part key={part.name} part={part} />
+    ))}
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+const Course = (props) => (
+  <>
+    <Header course={props.course.name} />
+    <Content parts={props.course.parts} />
+  </>
+)
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
