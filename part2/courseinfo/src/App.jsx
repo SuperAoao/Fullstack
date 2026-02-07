81f78764231d3d2d9c1c2906597d758dcf06cdd3
Ao Zhang <superaoao39@outlook.com>
2026-02-07 22:24:18 +1100

    2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,41 +1,5 @@
-const Header = (props) => <h1>{props.course}</h1>
+import Course from './Course'
 
-const Total = (props) => {
-  const total = props.parts.reduce((sum, part) => {
-    console.log("Whats happening", sum, part.exercises)
-    return sum + part.exercises
-  }, 0)
-  return (<p>
-    <strong>total of {total} exercises</strong>
-    </p>)
-}
-
-const Content = (props) => (
-  <div>
-    {props.parts.map((part) => (
-      <Part key={part.name} part={part} />
-    ))}
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Course = (props) => (
-  <>
-    {props.course.map(course => {
-      return (
-      <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-      </>)
-    })}
-  </>
-)
 const App = () => {
   const courses = [
     {
