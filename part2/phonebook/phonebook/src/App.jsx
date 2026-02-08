d8ceb02a30024d8326721b3a0936c07f63d6fa57
Ao Zhang <superaoao39@outlook.com>
2026-02-08 18:07:34 +1100

    2.6

---
+++ part2/phonebook/phonebook/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={(event) => {
+            event.preventDefault()
+            setNewName(event.target.value)}} />
+        </div>
+        <div>
+          <button type="submit" onClick={(event) => {
+            event.preventDefault()
+            const personObject = {
+              name: newName
+            }
+            setPersons(persons.concat(personObject))
+            setNewName('')
+          }}>add</button>
+        </div>
+        <div>debug: {newName}</div>
+      </form>
+      <h2>Numbers</h2>
+      <h3>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </h3>
+      ...
+    </div>
+  )
+}
+
+export default App
