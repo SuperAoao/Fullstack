0bae64e52fed5cb2c7d559ae1856f8e7bf7c36f0
Ao Zhang <superaoao39@outlook.com>
2026-02-09 22:47:38 +1100

    2.11

--- part2/phonebook/phonebook/src/App.jsx
+++ part2/phonebook/phonebook/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import PersonList from './PersonList'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [shownPersons, setShownPersons] = useState(persons)
+  const [persons, setPersons] = useState([])
+  const [shownPersons, setShownPersons] = useState([])
+
+  // Fetching data from the server
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      const persons = response.data
+      console.log(persons)
+      setPersons(persons)
+      setShownPersons(persons)
+    })
+  }, [])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   return (
